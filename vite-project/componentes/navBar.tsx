a9bc5ad873a2b9710bb02ffe41cff748759d7eb7
Vanessa <vs6615302@gmail.com>
2024-10-29 20:37:04 -0300

    fazendo alguns testes

--- vite-project/componentes/navBar.tsx
+++ vite-project/componentes/navBar.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export function NavBar() {
   return (
-    <div>
+    <>
       <nav>
           <div>
             <h1>NavBar</h1>
           </div>
         </nav>
-    </div>
+    </>
   );
 }
