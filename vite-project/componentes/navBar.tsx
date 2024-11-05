f3b4a29854de94e6d6c049a07bcfe0bf6fad19f8
Vanessa <vs6615302@gmail.com>
2024-11-04 21:18:30 -0300

    Alteração no arquivo

--- vite-project/componentes/navBar.tsx
+++ vite-project/componentes/navBar.tsx
@@ -1,13 +1,12 @@
+// src/components/NavBar.tsx
 //import React from 'react';
 
-export function NavBar() {
+export function NavBar(): JSX.Element {
   return (
-    <>
-      <nav>
-        <div>
-          <h1>NavBar</h1>
-         </div>
-      </nav>
-    </>
+    <nav>
+      <div>
+        <h1>NavBar</h1>
+      </div>
+    </nav>
   );
 }
