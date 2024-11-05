592eaea76bebdc9c58f014822f9ad2c51e92794c
Vanessa <vs6615302@gmail.com>
2024-11-04 21:19:54 -0300

    Teste

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,22 +1,18 @@
-// src/App.tsx
 import './App.css';
-import { NavBar } from './components/navBar'; // Importa o NavBar
+import { NavBar } from './components/NavBar';
 
 function App(): JSX.Element {
   return (
     <div className="app-container">
-      <NavBar /> {/* Exibe o NavBar */}
-
+      <NavBar />
       <section className="section">
         <div className="section-content">
           <h1>Section 1</h1>
         </div>
-
         <div className="section-content">
           <h2>Section 2</h2>
         </div>
       </section>
-
       <footer className="footer">
         <h1>Footer</h1>
       </footer>
