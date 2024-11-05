94f000980ac74e59cfbf742678e91a8a853ac3a8
Vanessa <vs6615302@gmail.com>
2024-11-05 20:31:02 -0300

    Alteração arquivo

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <>
-     <div>teste</div>
+    <nav>
+      <div>
+        <h1>NavBar</h1>
+      </div>
+    </nav>
+
+    <section>
+      <h1>
+        Section1
+      </h1>
+    </section>
+
+    <section>
+      <h1>section2</h1>
+    </section>
+    
+    <footer>
+      <h4>footer</h4>
+    </footer>
     </>
   );
 }
