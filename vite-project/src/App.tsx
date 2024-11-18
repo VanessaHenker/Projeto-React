4077251f2143b40d1117f812bc86790c06ecb743
Vanessa <vs6615302@gmail.com>
2024-11-18 17:20:10 -0300

    Alteração no cod

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,15 +6,7 @@ function App() {
     <>
     <NavBar/>
     <NavBar/>
-      <section>
-        <div>
-          <h1>Section 1</h1>
-        </div>
-
-        <div>
-          <h2>Section 2</h2>
-        </div>
-      </section>
+     
 
       <footer>
         <h1>Footer</h1>
