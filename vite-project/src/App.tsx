76e0a13b47ec2b0ac5d2b214c18e118904db3f8b
Vanessa <vs6615302@gmail.com>
2024-11-20 16:00:46 -0300

    Alteração no cod

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Login } from './pages/login';
 import { Home } from './pages/home';
+import { Footer } from './componentes/footer';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-       <Route path="/login"
-        element={<Login/>}>
-      </Route>
+        <Route path="/login"
+          element={<Login/>}>
+        </Route>
         
-      <Route path="/home"
-        element={<Home/>}>
-      </Route>
+        <Route path="/home"
+          element={<Home/>}>
+        </Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
