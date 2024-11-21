8829bce69ccb1e649be3339eb13ac274977f221b
Vanessa <vs6615302@gmail.com>
2024-11-21 14:04:16 -0300

    Teste

--- vite-project/src/componentes/sayMyName.tsx
+++ vite-project/src/componentes/sayMyName.tsx
@@ -1,15 +1,19 @@
-// Definindo a interface para as props
-interface SayMyNameProps {
-  nome: string; // 'nome' é do tipo string
+// Definindo uma interface para um objeto
+interface Pessoa {
+  nome: string;
+  idade: number;
 }
 
-// Função com as props tipadas
-function SayMyName(props: SayMyNameProps) {
-  return (
-    <>
-      <p>Fala aí {props.nome}, suave? </p>
-    </>
-  );
+// Função que usa a interface Pessoa
+function apresentarPessoa(pessoa: Pessoa): string {
+  return `Olá, meu nome é ${pessoa.nome} e eu tenho ${pessoa.idade} anos.`;
 }
 
-export default SayMyName;
+// Criando um objeto que segue a interface
+const pessoa: Pessoa = {
+  nome: "Vanessa",
+  idade: 30
+};
+
+// Chamando a função e exibindo o resultado
+console.log(apresentarPessoa(pessoa));
