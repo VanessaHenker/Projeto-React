27170b03c9764b4e29147b4b5dd48220b43415cb
Vanessa <vs6615302@gmail.com>
2024-11-21 18:15:54 -0300

    Alteração no cod

---
+++ vite-project/src/componentes/itens.tsx
@@ -0,0 +1,9 @@
+function Item(props: { marca: string }) {
+  return (
+    <>
+      <li>{props.marca}</li>
+    </>
+  );
+}
+
+export default Item;
--- vite-project/src/componentes/list.tsx
+++ vite-project/src/componentes/list.tsx
@@ -1,10 +1,12 @@
+import Item from "./itens";
+
 function List(){
   return(
     <>
       <h1>Minha Lista</h1>
       <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
+        <Item marca = "Ferrari"/>
+        <Item marca = "Fiat"/>
       </ul>
     </>
   );
