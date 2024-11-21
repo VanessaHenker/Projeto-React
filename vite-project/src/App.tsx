b9bb598466fa638e05a1f3059340f787ec75bb51
Vanessa <vs6615302@gmail.com>
2024-11-21 10:27:42 -0300

    Alteração no cod

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Footer } from './componentes/footer';
 import { AppRoutes } from './routes';
 
@@ -6,13 +6,21 @@ import { AppRoutes } from './routes';
 function App() {
   const name = 'Vanessa'
 
+  function soma(a, b) {
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <BrowserRouter>
-      <AppRoutes/>
-      <Footer/>
+      <AppRoutes />
+      <Footer />
 
+    {/*   Conteudo aula 2 */}
       <p>Olá, {name}</p>
-      <p>Soma: {2 + 2}</p>
+      <p>Soma: {soma(1, 2)}</p>
+      <img src={url} alt="MinhaImg" />
     </BrowserRouter>
   );
 }
