bdd61f8aa31c82ee5b6f69e677cf0ea50338f1cf
Vanessa <vs6615302@gmail.com>
2024-11-22 15:09:44 -0300

    teste

--- PaginaLogin/src/App.tsx
+++ PaginaLogin/src/App.tsx
@@ -1,17 +1,21 @@
 import './style.css';
-
-import Createa from "./componets/Createa"
-import Login from "./componets/Login"
+import { useState } from 'react';
+import Createa from "./componets/Createa";
+import Login from "./componets/Login";
 
 function App() {
+  const [mode, setMode] = useState("sign-in-js");
+
   return (
     <>
-      <main className="conteudo">
-       <Login/>
-       <Createa/>
+      <main className={`conteudo ${mode}`}>
+        <button id="signin" onClick={() => setMode("sign-in-js")}>Sign In</button>
+        <button id="signup" onClick={() => setMode("sign-up-js")}>Sign Up</button>
+        <Login />
+        <Createa />
       </main>
     </>
-  )
+  );
 }
-<script src = "script.js"></script>
-export default App
+
+export default App;
