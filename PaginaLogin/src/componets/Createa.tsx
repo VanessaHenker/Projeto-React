092ca7114b4eb607e2e2194b109b113afc4758b7
Vanessa <vs6615302@gmail.com>
2024-11-25 10:28:00 -0300

    Codigo atualizado

--- PaginaLogin/src/componets/Createa.tsx
+++ PaginaLogin/src/componets/Createa.tsx
@@ -1,42 +1,33 @@
-import React, { useState } from 'react';
+import IconSocial from './IconSocial';
+import FormLogin from './FormLogin';
 
 interface CreateaProps {
   onButtonClick: () => void;
 }
 
 const Createa: React.FC<CreateaProps> = ({ onButtonClick }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Create Account:', { email, password }); // Lógica para criar conta.
-  };
-
   return (
-    <div className="content conteudo-principal">
+    <div className="content conteudo-principal-secundario">
       <section className="conteudo-coluna-principal">
-        <h2 className="conteudo-titulo titulo-primario">Create Account</h2>
-        <form onSubmit={handleSubmit} className="conteudo-principal-form">
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit" className="btn button-primario">Sign up</button>
-        </form>
-        <button className="btn button-secundario" onClick={onButtonClick}>
-          Sign in
+        <h2 className="conteudo-titulo titulo-primario">Hello, Friend</h2>
+        <p className="conteudo-subtitulo descricao-primaria">Enter your personal details</p>
+        <p className="conteudo-subtitulo descricao-primaria">and start journey with us</p>
+        <button className="btn button-primario" onClick={onButtonClick}>
+          Sign up
         </button>
+      </section>
+
+      <section className="conteudo-coluna-secundario">
+        <h2 className="conteudo-titulo titulo-secundario">Sign in to developer</h2>
+        <IconSocial />
+        <p className="conteudo-subtitulo descricao-secundaria">or use your email account</p>
+        <form className="conteudo-principal-form">
+          <FormLogin />
+
+          <a className="password">forgot your password?</a>
+
+          <button className="btn button-secundario">sign in</button>
+        </form>
       </section>
     </div>
   );
