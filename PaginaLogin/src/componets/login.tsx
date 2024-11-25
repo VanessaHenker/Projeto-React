4a16b17cc8141f28ed13eeead9d39e0755b014c9
Vanessa <vs6615302@gmail.com>
2024-11-25 10:53:36 -0300

    c

--- PaginaLogin/src/componets/login.tsx
+++ PaginaLogin/src/componets/login.tsx
@@ -1,42 +1,32 @@
-import React, { useState } from 'react';
+import FormName from './FromName';
+import FormLogin from './FormLogin';
+import IconSocial from './IconSocial';
 
 interface LoginProps {
   onButtonClick: () => void;
 }
-
 const Login: React.FC<LoginProps> = ({ onButtonClick }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Login:', { email, password }); // Lógica de validação pode ser adicionada aqui.
-  };
-
   return (
     <div className="content conteudo-principal">
       <section className="conteudo-coluna-principal">
         <h2 className="conteudo-titulo titulo-primario">Welcome back!</h2>
-        <form onSubmit={handleSubmit} className="conteudo-principal-form">
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit" className="btn button-primario">Sign in</button>
-        </form>
-        <button className="btn button-secundario" onClick={onButtonClick}>
-          Create Account
+        <p className="conteudo-subtitulo descricao-primaria">To keep with your personal info</p>
+        <p className="conteudo-subtitulo descricao-primaria">Please login with personal info</p>
+        <button className="btn button-primario" onClick={onButtonClick}>
+          Sign in
         </button>
+      </section>
+
+      <section className="conteudo-coluna-secundario">
+        <h2 className="conteudo-titulo titulo-secundario">create account</h2>
+        <IconSocial />
+        <p className="conteudo-subtitulo descricao-secundaria">or use your email for registration: </p>
+        <form className="conteudo-principal-form">
+          <FormName />
+          <FormLogin />
+
+          <button className="btn button-secundario">sign up</button>
+        </form>
       </section>
     </div>
   );
