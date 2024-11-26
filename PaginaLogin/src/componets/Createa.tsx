2797f0bb47f13a9958ddb7deedb6126ad3bd1f43
Vanessa <vs6615302@gmail.com>
2024-11-26 15:47:17 -0300

    Alteração no cod

--- PaginaLogin/src/componets/Createa.tsx
+++ PaginaLogin/src/componets/Createa.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 
 
 interface CreateProps{
@@ -7,5 +7,35 @@ interface CreateProps{
 }
 
 const Createa: React.FC<CreateProps> = ({ onButtonClick,onCreateAccount}) => {
-  
+  const [email, setEmail], = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleCreateAccount = (e: React.FormEvent) =>{
+    e.preventDefault();
+    onCreateAccount(email, password);
+  }
+  return(
+    <div className="content conteudo-principal-secundario">
+      <section className="conteudo-coluna-principal">
+        <h2 className="conteudo-titulo titulo-primario">Hello, Friend</h2>
+        <p className="conteudo-subtitulo descricao-primaria">Enter your personal details</p>
+        <p className="conteudo-subtitulo descricao-primaria">and start journey with us</p>
+        <button className="btn button-primario" onClick={onButtonClick}>
+          Sign up
+        </button>
+      </section>
+
+      <section className="conteudo-coluna-secundario">
+        <h2 className="conteudo-titulo titulo-secundario">Create an account</h2>
+        <IconSocial />
+        <p className="conteudo-subtitulo descricao-secundaria">or use your email for registration</p>
+        <form className="conteudo-principal-form" onSubmit={handleCreateAccount}>
+          <FormName/>
+          <FormLogin setEmail={setEmail} setPassword={setPassword} />
+          <button className="btn button-secundario" type="submit">Sign up</button>
+        </form>
+      </section>
+    </div>
+      
+  );
 }
