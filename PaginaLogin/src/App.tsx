b044daa30fc197bc66a09d1554478445dcd1f88a
Vanessa <vs6615302@gmail.com>
2024-11-26 08:50:51 -0300

    Refazendo cod

--- PaginaLogin/src/App.tsx
+++ PaginaLogin/src/App.tsx
@@ -1,46 +1,13 @@
-import React, { useState } from 'react';
-import './style.css';
 
-import Createa from "./componets/Createa";
-import Login from "./componets/Login";
 
-const App: React.FC = () => {
-  const [isSignIn, setIsSignIn] = useState(false);
-
-  const handleButtonClick = () => {
-    setIsSignIn(!isSignIn);
-  };
-
-  const handleCreateAccount = (email: string, password: string) => {
-    const users = JSON.parse(localStorage.getItem("users") || "{}");
-    if (users[email]) {
-      alert("Email já cadastrado!");
-      return false;
-    }
-    users[email] = password;
-    localStorage.setItem("users", JSON.stringify(users));
-    alert("Conta criada com sucesso!");
-    return true;
-  };
-
-  const handleLogin = (email: string, password: string) => {
-    const users = JSON.parse(localStorage.getItem("users") || "{}");
-    if (users[email] && users[email] === password) {
-      alert("Login realizado com sucesso!");
-      return true;
-    }
-    alert("Email ou senha incorretos!");
-    return false;
-  };
-
+function App() {
   return (
-    <main className={isSignIn ? 'sign-in-js' : 'sign-up-js'}>
+    <main>
       <div className="conteudo">
-        <Login onButtonClick={handleButtonClick} onLogin={handleLogin} />
-        <Createa onButtonClick={handleButtonClick} onCreateAccount={handleCreateAccount} />
+        
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
