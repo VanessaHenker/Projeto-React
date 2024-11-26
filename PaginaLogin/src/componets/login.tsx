384203a18c0ece02426cf9c539cdb8f7a6a0064d
Vanessa <vs6615302@gmail.com>
2024-11-26 14:26:30 -0300

    Alt

--- PaginaLogin/src/componets/login.tsx
+++ PaginaLogin/src/componets/login.tsx
@@ -13,6 +13,19 @@ const Login: React.FC<LoginProps> = ({onButtonClick, onLogin}) =>{
     e.preventDefault();
     onLogin(email, password);
   };
+
+  return(
+    <div className="content conteudo-principal">
+      <section className="conteudo-coluna-principal">
+        <h2 className="conteudo-titulo titulo-primario">Welcome back!</h2>
+        <p className="conteudo-subtitulo descricao-primaria">To keep with your personal info</p>
+        <p className="conteudo-subtitulo descricao-primaria">Please login with personal info</p>
+        <button className="btn button-primario" onClick={onButtonClick}>
+          Sign in
+        </button>
+      </section>
+    </div>
+  );
 }
 
 
