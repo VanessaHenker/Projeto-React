dddb67059822f3fcbc39fe83c3327c003626e41d
Vanessa <vs6615302@gmail.com>
2024-12-03 20:22:12 -0300

    teste css

--- vite-project/src/componentes/form.tsx
+++ vite-project/src/componentes/form.tsx
@@ -6,8 +6,8 @@ function From() {
 
   return (
     <div>
-      <h1>Meu Cadastro:</h1>
-      <form onSubmit={cadastrarUsuario}>
+      <h1 className="teste">Meu Cadastro:</h1>
+      <form onSubmit={cadastrarUsuario} className="teste">
         <input type="text" placeholder="Digite o seu nome:" />
         <input type="submit" value="Cadastrar" />
       </form>
