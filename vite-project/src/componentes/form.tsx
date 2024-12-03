8fc3dfb55fa161440e31daafdf0f21ca393fb080
Vanessa <vs6615302@gmail.com>
2024-12-02 21:42:57 -0300

    teste

--- vite-project/src/componentes/form.tsx
+++ vite-project/src/componentes/form.tsx
@@ -1,7 +1,13 @@
 function Form() {
   return (
     <div>
-      
+      <h1>Meu cadastro: </h1>
+      <form action="">
+        <input type="text"  placeholder="Digite o seu nome:"/>
+      </form>
+      <div>
+        <input type="submit" value={cadastrar}/>
+      </div>
     </div>
   )
 }
