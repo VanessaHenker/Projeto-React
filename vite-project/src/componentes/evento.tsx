a6d3196050b2c830f2b3c4fac9f7844815c0b1fd
Vanessa <vs6615302@gmail.com>
2024-12-04 20:56:01 -0300

    teste

--- vite-project/src/componentes/evento.tsx
+++ vite-project/src/componentes/evento.tsx
@@ -1,13 +1,12 @@
 function Evento({ numero }: { numero: number }) {
-
   function meuEvento(_e: React.MouseEvent<HTMLButtonElement>) {
     alert(`Opa, fui ativado! Número recebido: ${numero}`);
   }
 
   return (
-    <div className='teste'>
+    <div className="teste">
       <p>Clique para disparar um evento</p>
-      <button text = "Primeiro evento JS" ></button>
+      <button>Primeiro evento</button>
       <button onClick={meuEvento}>Clique aqui</button>
     </div>
   );
