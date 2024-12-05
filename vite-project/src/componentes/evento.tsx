b273cd54fb3f59dc1438a2746500f3de5b8370a6
Vanessa <vs6615302@gmail.com>
2024-12-04 21:08:46 -0300

    Criando function segundoEvent

--- vite-project/src/componentes/evento.tsx
+++ vite-project/src/componentes/evento.tsx
@@ -6,9 +6,14 @@ function Evento({ numero }: { numero: number }) {
     alert(`Opa, fui ativado! Número recebido: ${numero}`);
   }
 
+  function segundoEvento(_e: React.MouseEvent<HTMLButtonElement>) {
+    alert(`Opa, fui ativado! Número recebido: ${numero}`);
+  }
+  
   return (
     <div className="teste">
       <p>Clique para disparar um evento</p>
+      <Button evento={meuEvento} text="Primeiro evento" />
       <Button evento={meuEvento} text="Primeiro evento" />
     </div>
   );
