5854a2bdf81db3f995c72c0d6d39d1f62782e1f3
Vanessa <vs6615302@gmail.com>
2024-12-07 16:21:35 -0300

    alteração no cod

--- vite-project/src/componentes/condicional.tsx
+++ vite-project/src/componentes/condicional.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 
 function Condicional() {
-  
-  const [email, setEmail] = useState();
-  const [userEmail, setUserEmail] = useState();
+  const [email, setEmail] = useState<string>("");
 
   function enviarEmail(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault(); 
-    alert("Tudo ok");
+    alert(`Email enviado: ${email}`);
   }
 
   return (
     <div>
       <h2>Cadastre seu email:</h2>
       <form onSubmit={enviarEmail}>
-        <input type="email" placeholder="Digite seu email" 
-        onChange={e => setEmail(e.target.value)}/>
+        <input 
+          type="email" 
+          placeholder="Digite seu email" 
+          value={email} 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} 
+        />
         <button type="submit">Enviar email</button>
       </form>
     </div>
