e6e0cde3d7b0d7398a54513f69574ac886d51bef
Vanessa <vs6615302@gmail.com>
2024-12-07 16:45:52 -0300

    Ultima alteração

--- vite-project/src/componentes/condicional.tsx
+++ vite-project/src/componentes/condicional.tsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
 
 function Condicional() {
-  const [email, setEmail] = useState<string>(""); 
-  const [userEmail, setUserEmail] = useState<string>(""); 
+  const [email, setEmail] = useState<string>("");
+  const [userEmail, setUserEmail] = useState<string>("");
 
   function enviarEmail(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault(); 
-    setUserEmail(email); 
+    e.preventDefault();
+    setUserEmail(email);
   }
 
 
-  function limparEmail(){
-   setUserEmail('')
+  function limparEmail() {
+    setUserEmail('')
   }
 
   return (
     <div>
       <h2>Cadastre seu email:</h2>
       <form onSubmit={enviarEmail}>
-        <input 
-          type="email" 
-          placeholder="Digite seu email" 
-          value={email} 
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} 
+        <input
+          type="email"
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
         />
         <button type="submit">Enviar email</button>
+        {userEmail &&(
+          <div>
+            {userEmail && <p>O email cadastrado é: {userEmail}</p>}
+            <button onClick={limparEmail}>Limpar email</button>
+          </div>
+        )}
+        
       </form>
-      {userEmail && <p>O email cadastrado é: {userEmail}</p>}
-      <button onClick={limparEmail}>Limpar email</button>
-    </div>
+      </div>
   );
 }
 
