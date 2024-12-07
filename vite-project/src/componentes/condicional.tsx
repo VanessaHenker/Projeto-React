716913e6e42a0dfc3c9cfa74515c1f547abecbf2
Vanessa <vs6615302@gmail.com>
2024-12-06 22:21:17 -0300

    teste

--- vite-project/src/componentes/condicional.tsx
+++ vite-project/src/componentes/condicional.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react"
+
 
 function Condicional() {
+  function enviarEmail(e) {
+    e.preventDefault(); 
+    alert("Tudo ok");
+  }
+
   return (
     <div>
       <h2>Cadastre seu email:</h2>
-      <form>
+      <form onSubmit={enviarEmail}>
         <input type="email" placeholder="Digite seu email" />
+        <button type="submit">Enviar email</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Condicional
+export default Condicional;
