127b00ec43ba04bb98893204c1e4799a481cc6dd
Vanessa <vs6615302@gmail.com>
2024-12-10 20:09:06 -0300

    Alteração no cod

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     <div className="teste">
       <h1>State lift</h1>
       <SeuNome setNome = {setNome}/>
+      
     </div>
   );
 }
--- vite-project/src/componentes/seuNome.tsx
+++ vite-project/src/componentes/seuNome.tsx
@@ -2,7 +2,7 @@ function SeuNome(setNome) {
   return (
     <div>
       <p>Digite seu nome:</p>
-      <input type="text"  placeholder='Qaul é o seu nome?'/>
+      <input type="text"  placeholder='Qaul é o seu nome?' onChange={(e) => setNome(e.target.value)}/>
     </div>
   )
 }
