5449d6289e42583572b68a5b54c0c0e2bea37e7e
Vanessa <vs6615302@gmail.com>
2024-12-09 21:20:01 -0300

    alteração no cod

--- vite-project/src/componentes/lista.tsx
+++ vite-project/src/componentes/lista.tsx
@@ -1,12 +1,12 @@
-
-
-function Lista({lista}) {
+function Lista({ lista }) {
   return (
     <>
       <h3>Lista de coisas boas:</h3>
-      
+      {lista.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
     </>
-  )
+  );
 }
 
-export default Lista
+export default Lista;
