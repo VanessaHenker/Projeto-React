5ebf3ed018f584735359bb1426bd8fb79c12949a
Vanessa <vs6615302@gmail.com>
2024-12-10 20:02:27 -0300

    Criando arquivo seuNome.tsx

---
+++ vite-project/src/componentes/seuNome.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SeuNome() {
+  return (
+    <div>
+      <p>Digite seu nome:</p>
+      <input type="text"  placeholder='Qaul é o seu nome?'/>
+    </div>
+  )
+}
+
+export default SeuNome
