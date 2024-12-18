428f9df205b9752102cc5ca831af9eb3491510f8
Vanessa <vs6615302@gmail.com>
2024-12-18 20:45:24 -0300

    Alteração no cod

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,19 +1,13 @@
 // App.js
 import './app.css';
-import { useState } from 'react';
-import SeuNome from './componentes/seuNome';
-import Saudacao from './componentes/saudacao';
+
 
 function App() {
-  const [nome, setNome] = useState('');
+
 
   return (
-    <div className="teste">
-      <h1>State lift</h1>
-      <SeuNome setNome={setNome} />
-      {nome && <p>Bem-vindo, {nome}!</p>}
-      <Saudacao nome = {nome}/>
-    </div>
+    <>
+    </>
   );
 }
 
