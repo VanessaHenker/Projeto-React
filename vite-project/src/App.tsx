d552ad91f18ef88f9fd357a17b2e302194100a5e
Vanessa <vs6615302@gmail.com>
2024-12-18 21:23:53 -0300

    Criando arquivo homee.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
     <Router>
       <ul>
         <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/Empresa'}>Empresa</Link></li>
+        <li><Link to={'/Contato'}>Contato</Link></li>
       </ul>
     </Router>
   );
---
+++ vite-project/src/pages/homee.tsx
@@ -0,0 +1,12 @@
+
+
+function Homee() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Conteudo da pagina</p>
+    </div>
+  )
+}
+
+export default Homee
