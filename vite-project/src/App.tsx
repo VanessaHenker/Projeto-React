1ca5c415e86046c6c616c2ab25838e4cad71f2bd
Vanessa <vs6615302@gmail.com>
2024-12-18 21:34:18 -0300

    Alteração no cod

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,37 +1,24 @@
-// App.js
 import './app.css';
-import { BrowserRouter as Router ,Switch, Route, Link} from 'react-router-dom';
-import Homee from './pages/homee';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/homee';
 import Empresas from './pages/empresas';
 import Contato from './pages/contato';
 
-
 function App() {
-
-
   return (
     <Router>
       <ul>
-        <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'/Empresa'}>Empresa</Link></li>
-        <li><Link to={'/Contato'}>Contato</Link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/empresas">Empresa</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
       </ul>
 
-      <Switch>
-        <Route path = '/'>
-          <Homee/>
-        
-         
-        </Route>
-      </Switch>
-      <Route path='/empresas'>
-      <Empresas/>
-      </Route>
-      <Route path='/contato'>
-      <Contato/>
-      </Route>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresas" element={<Empresas />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
     </Router>
-
   );
 }
 
