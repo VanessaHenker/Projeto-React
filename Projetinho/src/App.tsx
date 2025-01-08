53931ca6abcff6d240652060748e2607699af9d8
Vanessa <vs6615302@gmail.com>
2025-01-08 20:26:31 -0300

    Alteração no cod

--- Projetinho/src/App.tsx
+++ Projetinho/src/App.tsx
@@ -1,33 +1,29 @@
 import './style.css';
-import { BrowserRouter as Router, Routes, Route, Switch, Link } from 'react-router-dom'
 
-import Home from './componets/home';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import Company from './componets/company';
 import Contact from './componets/contact';
+import Home from './componets/home';
 import Newproject from './componets/newproject';
 
 
 function App() {
   return (
     <Router>
-        <div>
-          <Link to = "/">Home</Link>
-          <Link to = "/contact">Contato</Link>
-          <Link to = "/company">Empresa</Link>
-          <Link to = "/newproject">Novo projeto</Link>
-        </div>
-    
-      
-      <Switch>
-        <div>
-          
-        </div>
-      </Switch>
-      
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">Novo projeto</Link>
+        </nav>
+      </div>
+
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/company" element={<Company/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/company" element={<Contact/>} />
+        <Route path="/contact" element={<Company/>} />
         <Route path="/newproject" element={<Newproject/>} />
       </Routes>
     </Router>
