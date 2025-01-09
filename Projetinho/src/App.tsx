61a3a1fc9d88dff8ccff87324e87def8dac5d71a
Vanessa <vs6615302@gmail.com>
2025-01-09 19:38:12 -0300

    Teste

--- Projetinho/src/App.tsx
+++ Projetinho/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Company from './components/company';
 import Contact from './components/contact';
 import Home from './components/home';
-import Newproject from './components/newproject';
+import NewProject from './components/newproject';
 import Container from './components/layout/container';
 
 function App() {
@@ -13,17 +13,19 @@ function App() {
     <Router>
       <Container>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contato</Link>
-          <Link to="/company">Empresa</Link>
-          <Link to="/newproject">Novo projeto</Link>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contact">Contato</Link></li>
+            <li><Link to="/company">Empresa</Link></li>
+            <li><Link to="/newproject">Novo projeto</Link></li>
+          </ul>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/newproject" element={<Newproject />} />
+          <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
     </Router>
