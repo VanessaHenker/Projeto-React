66dbdafea16e46a0225c5a845ac347826785160b
Vanessa <vs6615302@gmail.com>
2025-01-09 19:35:25 -0300

    Alteração no cod

--- Projetinho/src/components/layout/container.tsx
+++ Projetinho/src/components/layout/container.tsx
@@ -1,12 +1,12 @@
-import styles from './containerModules.module.css';
-import React from 'react';
+import styles from './containerModules.module.css'
+import React from 'react'
 
 interface ContainerProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 function Container({ children }: ContainerProps) {
-  return <div className={styles.container}>{children}</div>;
+  return <div className={styles.container}>{children}</div>
 }
 
-export default Container;
+export default Container
