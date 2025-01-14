47556b46ae2a0d310a1815eb01745f6e2e37067c
Vanessa <vs6615302@gmail.com>
2025-01-14 19:19:55 -0300

    Alteração no cod

--- Projetinho/src/components/layout/navBar.tsx
+++ Projetinho/src/components/layout/navBar.tsx
@@ -1,20 +1,16 @@
 import { Link } from 'react-router-dom';
 
 
-/* import styles from './layout/navBar.module.css';
-import logo from '../../img/costs_logo.png';
-import Container from './container'; */
+
 
 function NavBar() {
   return (
-    <div>
-      <nav>
-        <li><Link to="/">Home</Link></li>
+    <nav>
+      <li><Link to="/">Home</Link></li>
        <li><Link to="/contact">Contato</Link></li>
        <li><Link to="/company">Empresa</Link></li>
        <li><Link to="/newproject">Novo projeto</Link></li>
-      </nav>
-    </div>
+    </nav>
   )
 }
   
