8dc8aa9debb7819bb83dd32db5bfed0e1cf80a14
Vanessa <vs6615302@gmail.com>
2025-01-14 19:37:27 -0300

    Alteração no cod

--- Projetinho/src/components/layout/navBar.tsx
+++ Projetinho/src/components/layout/navBar.tsx
@@ -1,28 +1,34 @@
-import { Link } from 'react-router-dom';
-import Container from './container';
+import { Link } from 'react-router-dom'
+import Container from './container'
 
 
 import logo from "../../img/costs_logo.png"
-import styles from "./navBar.module.css"
+import styles from "../layout/navBar.module.css";
+
 
 
 function NavBar() {
   return (
-    <nav>
+    <nav className= {styles.navBar}>
       <Container>
-        <Link to= "/">
-            <img src = {logo} alt = "costs" />
+        <Link to="/">
+          <img src={logo} alt="costs" />
         </Link>
-     <Link to="/">Home</Link>
-      <Link to="/contact">Contato</Link>
-      <Link to="/company">Empresa</Link>
-      <Link to="/newproject">Novo projeto</Link>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contato</Link></li>
+          <li><Link to="/company">Empresa</Link></li>
+          <li> <Link to="/newproject">Novo projeto</Link></li>
+        </ul>
+    
+     
+       
       </Container>
 
     </nav>
   )
 }
-  
+
 
 
 export default NavBar;
