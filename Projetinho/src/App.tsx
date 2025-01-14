d9d995769108c1b291052c0a14acc38cbc72387b
Vanessa <vs6615302@gmail.com>
2025-01-14 20:15:27 -0300

    Alteração no cod

--- Projetinho/src/App.tsx
+++ Projetinho/src/App.tsx
@@ -9,6 +9,7 @@ import NewProject from './components/newproject';
 import Container from './components/layout/container';
 import Footer from './components/footer';
 import NavBar from './components/layout/navBar';
+import Projects from './pages/projects';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Container customClass='min-height'>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path = "/projects" element = {<Projects/>}/>
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
--- Projetinho/src/components/layout/navBar.tsx
+++ Projetinho/src/components/layout/navBar.tsx
@@ -1,34 +1,27 @@
-import { Link } from 'react-router-dom'
-import Container from './container'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from './container';
 
+import logo from '../../img/costs_logo.png';
+import styles from '../layout/navBar.module.css';
 
-import logo from "../../img/costs_logo.png"
-import styles from "../layout/navBar.module.css";
-
-
-
-function NavBar() {
+const NavBar: React.FC = () => {
   return (
-    <nav className= {styles.navBar}>
+    <nav className={styles.navBar}>
       <Container>
         <Link to="/">
-          <img src={logo} alt="costs" />
+          <img src={logo} alt="Costs Logo" />
         </Link>
-        <ul className= {styles.list}>
+        <ul className={styles.list}>
           <li className={styles.item}><Link to="/">Home</Link></li>
+          <li className={styles.item}><Link to="/projects">Projetos</Link></li>
           <li className={styles.item}><Link to="/contact">Contato</Link></li>
-          <li className={styles.item}><Link to="/company">Empresa</Link></li>
-          <li className={styles.item}> <Link to="/newproject">Novo projeto</Link></li>
+          <li className={styles.item}><Link to="/company">Empresa</Link>
+          </li><li className={styles.item}><Link to="/newproject">Novo Projeto</Link></li>
         </ul>
-    
-     
-       
       </Container>
-
     </nav>
-  )
-}
-
-
+  );
+};
 
 export default NavBar;
