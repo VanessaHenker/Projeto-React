0134b94e2c962e54c6d43a8f231ed75666071112
Vanessa <vs6615302@gmail.com>
2025-01-15 18:42:52 -0300

    teste

--- Projetinho/src/pages/Home.tsx
+++ Projetinho/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import styles from './home.module.css';
+
 import savings from '../img/savings.svg'
 
 
 function Home() {
   return (
-    <section className={styles.home}>
+    <section >
       <h1>Bem-vindo ao <span>Costs</span></h1>
       <p>Comece a gerenciar os seus projetos agor mesmo!</p>
       <a href="/">Criar projeto</a>
