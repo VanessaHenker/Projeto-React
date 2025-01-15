30c7c7018268915ec32a2cc862b9baf198f749df
Vanessa <vs6615302@gmail.com>
2025-01-15 19:25:57 -0300

    Alteração no cod

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -1,11 +1,15 @@
-
+import savings from '../img/savings.svg'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <section >
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agor mesmo!</p>
+      <a href="/">Criar projeto</a>
+
+      <img src={savings} alt="Costs" />
+    </section>
+  );
 }
 
-export default Home
+export default Home;
--- Projetinho/src/pages/Home.tsx
+++ Projetinho/src/pages/Home.tsx
@@ -1,16 +1,6 @@
-
-import savings from '../img/savings.svg'
-
-
 function Home() {
   return (
-    <section >
-      <h1>Bem-vindo ao <span>Costs</span></h1>
-      <p>Comece a gerenciar os seus projetos agor mesmo!</p>
-      <a href="/">Criar projeto</a>
-
-      <img src={savings} alt="Costs" />
-    </section>
+    <h1>Home</h1>
   );
 }
 
