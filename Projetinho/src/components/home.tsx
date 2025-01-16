96c16c61450d30799a1acca135923cc22b524aba
Vanessa <vs6615302@gmail.com>
2025-01-16 20:37:21 -0300

    Teste

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -1,14 +1,17 @@
-import styles from './home.module.css' 
-import savings from '../img/savings.svg'
+import styles from './home.module.css';
+import savings from '../img/savings.svg';
 
 function Home() {
   return (
-    <section className={styles.homeContainer} >
-      <h1>Bem-vindo ao <span>Costs</span></h1>
-      <p>Comece a gerenciar os seus projetos agor mesmo!</p>
-      <a href="/">Criar projeto</a>
-
-      <img src={savings} alt="Costs" />
+    <section className={styles.homeContainer}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <img src={savings} alt="Imagem de economia representando savings" />
+      <a href="/" className={styles.createProjectButton}>
+        Criar Projeto
+      </a>
     </section>
   );
 }
