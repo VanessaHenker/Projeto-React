7773697afe54834878e434fcb614c9c1e8e64637
Vanessa <vs6615302@gmail.com>
2025-01-16 20:54:24 -0300

    Alteração no cod

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -1,12 +1,13 @@
 import styles from './layout/home.module.css';
 import savings from '../img/savings.svg';
+import LinkButton from './layout/linkButton';
 
 function Home() {
   return (
     <section className={styles.homeContainer}>
       <h1> Bem-vindo ao <span>Costs</span></h1>
       <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-      <a href="/" className={styles.createProjectButton}>Criar Projeto</a>
+      <LinkButton/>
       <img src={savings} alt="Imagem de economia representando savings" />
     </section>
   );
--- Projetinho/src/components/layout/linkButton.tsx
+++ Projetinho/src/components/layout/linkButton.tsx
@@ -3,7 +3,7 @@ import styles from './layout/LinkButton.module.css'
 
 const LinkButton = (to, text) => {
   return (
-   <Link  className='' {to = {to}}>
+   <Link  className= { styles.btn  to = {to}}>
     {text}
    </Link>
 
