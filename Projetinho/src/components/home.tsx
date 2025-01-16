67ef57764917d01db8e0a744b6ced72eb96ed57f
Vanessa <vs6615302@gmail.com>
2025-01-16 20:47:12 -0300

    Alteração no cod

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -4,14 +4,10 @@ import savings from '../img/savings.svg';
 function Home() {
   return (
     <section className={styles.homeContainer}>
-      <h1>
-        Bem-vindo ao <span>Costs</span>
-      </h1>
+      <h1> Bem-vindo ao <span>Costs</span></h1>
       <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <a href="/" className={styles.createProjectButton}>Criar Projeto</a>
       <img src={savings} alt="Imagem de economia representando savings" />
-      <a href="/" className={styles.createProjectButton}>
-        Criar Projeto
-      </a>
     </section>
   );
 }
