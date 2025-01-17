a3c96a2c41d0f8b5f243b7834f28b7704ecaf557
Vanessa <vs6615302@gmail.com>
2025-01-16 21:00:02 -0300

    Alteração no cod

--- Projetinho/src/components/layout/linkButton.tsx
+++ Projetinho/src/components/layout/linkButton.tsx
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom'
-import styles from './layout/LinkButton.module.css'
+import { Link } from 'react-router-dom';
+import styles from './layout/LinkButton.module.css';
 
-const LinkButton = (to, text) => {
-  return (
-   <Link  className= { styles.btn  to = {to}}>
-    {text}
-   </Link>
+const LinkButton = ({ to, text }) => {
+    return (
+        <Link className={styles.btn} to={to}>
+            {text}
+        </Link>
+    );
+};
 
-  )
-}
-
-export default LinkButton
+export default LinkButton;
