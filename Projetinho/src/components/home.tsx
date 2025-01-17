a46a9c9f94eee0acc109c7b81bd631307a573bee
Vanessa <vs6615302@gmail.com>
2025-01-17 19:48:36 -0300

    teste

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
-import styles from './LinkButton.module.css'; // Caminho corrigido
+import styles from './LinkButton.module.css';
 
 interface LinkButtonProps {
-  to: string;
-  text: string;
+  to: string; 
+  text: string; 
 }
 
 const LinkButton = ({ to, text }: LinkButtonProps) => {
   return (
     <Link to={to} className={styles.btn}>
-      {text}
-      <h1>ola</h1>
+      <span>{text}</span>
     </Link>
   );
 };
