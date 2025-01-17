8e4248258a59fa62f650e7acc863653619bce9bf
Vanessa <vs6615302@gmail.com>
2025-01-16 21:10:09 -0300

    Ultima alteração

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -1,17 +1,18 @@
-import styles from './layout/home.module.css';
-import savings from '../img/savings.svg';
-import LinkButton from './layout/linkButton';
+import { Link } from 'react-router-dom';
+import styles from './LinkButton.module.css'; // Caminho corrigido
 
-function Home() {
-  return (
-    <section className={styles.homeContainer}>
-      <h1> Bem-vindo ao <span>Costs</span></h1>
-      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-    
-      <LinkButton to="/newproject" text="Criar Projeto" />
-      <img src={savings} alt="Costs"/>
-    </section>
-  );
+interface LinkButtonProps {
+    to: string;
+    text: string;
 }
 
-export default Home;
+const LinkButton = ({ to, text }: LinkButtonProps) => {
+    return (
+        <Link to={to} className={styles.btn}>
+            {text}
+           <h1>ola</h1>
+        </Link>
+    );
+};
+
+export default LinkButton;
