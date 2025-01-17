90ebe8037497b47d6e87cba90765b6344945e550
Vanessa <vs6615302@gmail.com>
2025-01-17 19:52:49 -0300

    Alteração no cod

---
+++ Projetinho/src/components/home.tsx
@@ -0,0 +1,16 @@
+import styles from './home.module.css' 
+import savings from '../img/savings.svg'
+
+function Home() {
+  return (
+    <section className={styles.homeContainer} >
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agor mesmo!</p>
+      <a href="/">Criar projeto</a>
+
+      <img src={savings} alt="Costs" />
+    </section>
+  );
+}
+
+export default Home;
