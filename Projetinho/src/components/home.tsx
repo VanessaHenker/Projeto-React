90edd2ca55c08eb80452492a01f25bcdf94e0880
Vanessa <vs6615302@gmail.com>
2025-01-17 21:01:37 -0300

    Ultima alteração

--- Projetinho/src/components/home.tsx
+++ Projetinho/src/components/home.tsx
@@ -1,12 +1,14 @@
 import styles from './layout/home.module.css' 
 import savings from '../img/savings.svg'
+import LinkButton from './layout/linkButton';
 
 function Home() {
   return (
     <section className={styles.homeContainer} >
       <h1>Bem-vindo ao <span>Costs</span></h1>
       <p>Comece a gerenciar os seus projetos agor mesmo!</p>
-      <a href="/">Criar projeto</a>
+      
+      <LinkButton to = "/newproject" text = "Criar Projeto"/>
 
       <img src={savings} alt="Costs" />
     </section>
