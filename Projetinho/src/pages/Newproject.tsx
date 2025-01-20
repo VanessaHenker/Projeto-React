1bfd6752b0bb81467e8eaa0ce22f8f72dc66af42
Vanessa <vs6615302@gmail.com>
2025-01-20 20:12:20 -0300

    Alteração no cod

--- Projetinho/src/pages/Newproject.tsx
+++ Projetinho/src/pages/Newproject.tsx
@@ -1,9 +1,9 @@
-import styles from './pages/newProject.css'
+import styles from './newProject.css';
 
 function Newproject (){
   
   return (
-    <div className = {styles.newProjectContainer}>
+    <div className={styles.newProjectContainer}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
       <p>formulário</p>
