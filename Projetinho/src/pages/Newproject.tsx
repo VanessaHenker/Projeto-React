a8e39615d5be82a243a16bbf74347b96d35c087d
Vanessa <vs6615302@gmail.com>
2025-01-20 20:04:59 -0300

    Alteração no cod

--- Projetinho/src/pages/Newproject.tsx
+++ Projetinho/src/pages/Newproject.tsx
@@ -3,6 +3,8 @@ function Newproject (){
   return (
     <div>
       <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <p>formulário</p>
     </div>
 
   )
