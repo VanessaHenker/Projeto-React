45c3499a74f21d8f30e81b72bb9df302c9c1b931
Vanessa <vs6615302@gmail.com>
2025-01-21 20:52:33 -0300

    Alteração no cod para atualizações

--- Projetinho/src/components/projects/projectForm.tsx
+++ Projetinho/src/components/projects/projectForm.tsx
@@ -1,26 +1,39 @@
 import Input from '../form/input';
-import styles from './projectForm.module.css'
+import styles from './projectForm.module.css';
 
 function ProjectForm() {
   return (
-    <form className= {styles.form}>
-      <Input type = "text" text='Nome do projeto' name='name' placeholder='Insira o nome do proejto' />
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
 
-      <div>
-        <label htmlFor="budget">Orçamento Total:</label>
-        <input type="number" id="budget" placeholder="Insira o orçamento total" />
-      </div>
+      <Input
+        type="number"
+        text="Orçamento Total"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
 
-      <div>
+      <div className={styles.formControl}>
         <label htmlFor="category">Categoria:</label>
         <select name="category" id="category" defaultValue="">
-          <option disabled selected >Selecione a categoria</option>
-          
+          <option value="" disabled>
+            Selecione a categoria
+          </option>
+          <option value="design">Design</option>
+          <option value="development">Desenvolvimento</option>
+          <option value="marketing">Marketing</option>
         </select>
       </div>
 
-      <div>
-       
+      <div className={styles.formControl}>
+        <button type="submit" className={styles.btn}>
+          Criar Projeto
+        </button>
       </div>
     </form>
   );
