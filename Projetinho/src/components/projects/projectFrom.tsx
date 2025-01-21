baea288fb715c5a921097d99726a10e8c5170d1e
Vanessa <vs6615302@gmail.com>
2025-01-21 20:08:08 -0300

    Alteração no cod

--- Projetinho/src/components/projects/projectFrom.tsx
+++ Projetinho/src/components/projects/projectFrom.tsx
@@ -1,9 +1,13 @@
 
 function ProjectFrom() {
   return (
-    <>
-      <p>Form do projeto</p>
-    </>
+    <form action="">
+      <input type="text" placeholder="Insira o nome do projeto" />
+      <input type="number"  placeholder="Insira o orçamento total"/>
+      <select name="category">
+        <option disabled>Selecione a categoria</option>
+      </select>
+    </form>
   )
 }
 
