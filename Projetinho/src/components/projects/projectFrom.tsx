08fdf00aebcd44cfa97e72e4883932d50002262f
Vanessa <vs6615302@gmail.com>
2025-01-21 20:09:34 -0300

    Alteração no cod

--- Projetinho/src/components/projects/projectFrom.tsx
+++ Projetinho/src/components/projects/projectFrom.tsx
@@ -2,8 +2,14 @@
 function ProjectFrom() {
   return (
     <form action="">
-      <input type="text" placeholder="Insira o nome do projeto" />
-      <input type="number"  placeholder="Insira o orçamento total"/>
+      <div>
+        <input type="text" placeholder="Insira o nome do projeto" />
+      </div>
+
+      <div>
+        <input type="number" placeholder="Insira o orçamento total" />
+      </div>
+
       <select name="category">
         <option disabled>Selecione a categoria</option>
       </select>
