a5c1c238b8c5bc0e8fcea300de68fbbd93fedfed
Vanessa <vs6615302@gmail.com>
2025-01-23 20:49:09 -0300

    Alteração no cod

--- Projetinho/src/components/form/select.tsx
+++ Projetinho/src/components/form/select.tsx
@@ -1,21 +1,21 @@
-import styles from './input.module.css';
+import styles from './select.module.css';
 
 interface Option {
   value: string;
   label: string;
 }
 
-interface InputProps {
+interface SelectProps {
   type: string; 
   text: string;
   name: string;
   placeholder?: string; 
-  handleOnChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+  handleOnChange: (e: React.ChangeEvent<HTMLSelectElement | HTMLSelectElement>) => void;
   value?: string | number;
   options?: Option[]; 
 }
 
-function Input({ type, text, name, placeholder, handleOnChange, value, options }: InputProps) {
+function Select({ type, text, name, placeholder, handleOnChange, value, options }: SelectProps) {
   return (
     <div className={styles.formControl}>
       <label htmlFor={name}>{text}</label>
@@ -41,48 +41,4 @@ function Input({ type, text, name, placeholder, handleOnChange, value, options }
   );
 }
 
-export default Input;
-import styles from './input.module.css';
-
-interface Option {
-  value: string;
-  label: string;
-}
-
-interface InputProps {
-  type: string; 
-  text: string;
-  name: string;
-  placeholder?: string; 
-  handleOnChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
-  value?: string | number;
-  options?: Option[]; 
-}
-
-function Input({ type, text, name, placeholder, handleOnChange, value, options }: InputProps) {
-  return (
-    <div className={styles.formControl}>
-      <label htmlFor={name}>{text}</label>
-      {type === 'select' && options ? (
-        <select id={name} name={name} onChange={handleOnChange} value={value || ''}>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      ) : (
-        <input
-          type={type}
-          name={name}
-          id={name}
-          placeholder={placeholder}
-          onChange={handleOnChange}
-          value={value}
-        />
-      )}
-    </div>
-  );
-}
-
-export default Input;
+export default Select;
