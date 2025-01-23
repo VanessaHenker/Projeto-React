8377f39e0645414164a46cd7861b2eafad4d7f46
Vanessa <vs6615302@gmail.com>
2025-01-23 20:17:31 -0300

    Alteração no cod

--- Projetinho/src/components/form/input.tsx
+++ Projetinho/src/components/form/input.tsx
@@ -1,26 +1,33 @@
 import styles from './input.module.css';
 
-interface InputProps {
-  type: string;
-  text: string;
-  name: string;
-  placeholder: string;
-  handleOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  value?: string | number;
-}
-
-function Input({ type, text, name, placeholder, handleOnChange, value }: InputProps) {
+function Input({
+  type,
+  text,
+  name,
+  placeholder,
+  options,
+  handleOnChange,
+}: InputProps) {
   return (
-    <div className={styles.formControl}>
+    <div className={styles.inputControl}>
       <label htmlFor={name}>{text}</label>
-      <input
-        type={type}
-        name={name}
-        id={name}
-        placeholder={placeholder}
-        onChange={handleOnChange}
-        value={value}
-      />
+      {type === 'select' && options ? (
+        <select id={name} name={name} onChange={handleOnChange}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input
+          type={type}
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          onChange={handleOnChange}
+        />
+      )}
     </div>
   );
 }
