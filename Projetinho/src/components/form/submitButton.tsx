b3a518457ea35d1ebd439ad0607cb9a53bf766ca
Vanessa <vs6615302@gmail.com>
2025-01-27 20:08:30 -0300

    Alteração no submitButton

--- Projetinho/src/components/form/submitButton.tsx
+++ Projetinho/src/components/form/submitButton.tsx
@@ -3,9 +3,9 @@ import styles from './submitButton.module.css'
 function SubmitButton({text}) {
   return (
     <div>
-      <label htmlFor="">
-        
-      </label>
+      <button className= {styles.btn}>{text}</button>
+
+      
     </div>
   )
 }
