47b83429c6f2ac2b56fbb680f297308f8da6675b
Vanessa <vs6615302@gmail.com>
2025-01-28 20:51:25 -0300

    Alteração no newProject

--- Projetinho/src/pages/Newproject.tsx
+++ Projetinho/src/pages/Newproject.tsx
@@ -1,30 +1,37 @@
-import {useHistory} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-import ProjectFrom from '../components/projects/projectForm';
+import ProjectForm from '../components/projects/projectForm';
 import styles from './newProject.module.css';
 
-function Newproject (){
+function NewProject() {
+  const navigate = useNavigate();
 
-  const history = useHistory()  
-
-  function crearePost(project){
+  function createPost(project) {
     project.cost = 0;
-    project.service = []
+    project.service = [];
 
-    
-
-
+    fetch("http://localhost:5000/projects", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("Projeto criado:", data);
+        navigate("/projetos"); // Redireciona após criação
+      })
+      .catch((err) => console.error("Erro ao criar projeto:", err));
   }
 
-  
   return (
     <div className={styles.newProjectContainer}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectFrom/>
+      <ProjectForm onSubmit={createPost} />
     </div>
-
-  )
+  );
 }
 
-export default Newproject
+export default NewProject;
