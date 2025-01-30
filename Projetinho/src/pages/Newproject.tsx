7afba0e0c770301ce782052f468f22e4355fec41
Vanessa <vs6615302@gmail.com>
2025-01-29 21:49:50 -0300

    Criando arquivo db.json

--- Projetinho/src/pages/Newproject.tsx
+++ Projetinho/src/pages/Newproject.tsx
@@ -1,29 +1,10 @@
-import { useHistory } from 'react-router-dom';
+//import { useHistory } from 'react-router-dom';
 
 import ProjectForm from '../components/projects/projectForm';
 import styles from './newProject.module.css';
 
 function NewProject() {
 
-  const history = useHistory()
-
-  function createPost(project){
-      //initialize cost and service
-      project.cost = 0
-      project.service = []
-
-      fetch("http:/localhost:5000/project", {
-      method: 'POST',
-      headers: {
-        'Cpntent-type': 'application/json',
-      },
-    }).then(),catch(resp => Response.json )
-    .then((data) => {
-      //rediret
-    })
-      .catch(err => console.log(err))
-  }
-  
   return (
     <div className={styles.newProjectContainer}>
       <h1>Criar Projeto</h1>
