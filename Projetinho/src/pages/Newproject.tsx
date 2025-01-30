99f3ba5f8f73a86b9bff5324852e7a98c332a5c4
Vanessa <vs6615302@gmail.com>
2025-01-29 21:31:27 -0300

    Alteração no Newproject

--- Projetinho/src/pages/Newproject.tsx
+++ Projetinho/src/pages/Newproject.tsx
@@ -1,36 +1,34 @@
-import { useNavigate } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import ProjectForm from '../components/projects/projectForm';
 import styles from './newProject.module.css';
 
 function NewProject() {
-  const navigate = useNavigate();
 
-  function createPost(project) {
-    project.cost = 0;
-    project.service = [];
+  const history = useHistory()
 
-    fetch("http://localhost:5000/projects", {
+  function createPost(project){
+      //initialize cost and service
+      project.cost = 0
+      project.service = []
+
+      fetch("http:/localhost:5000/project", {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Cpntent-type': 'application/json',
       },
-      body: JSON.stringify(project)
+    }).then(),catch(resp => Response.json )
+    .then((data) => {
+      //rediret
     })
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log("Projeto criado:", data);
-        navigate("/projetos"); // Redireciona após criação
-      })
-      .catch((err) => console.error("Erro ao criar projeto:", err));
+      .catch(err => console.log(err))
   }
-
+  
   return (
     <div className={styles.newProjectContainer}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      {/* Passa a função `createPost` para o formulário */}
-      <ProjectForm onSubmit={createPost} />
+      <ProjectForm/>
     </div>
   );
 }
