c9d67e8437423295b139724920585a9b06ce77ae
Vanessa <vs6615302@gmail.com>
2025-02-07 19:00:30 -0300

    ULtima aplicação no project e message

--- Projetinho/src/pages/projects.tsx
+++ Projetinho/src/pages/projects.tsx
@@ -1,13 +1,12 @@
-import Message from "../components/layout/message"
-
+import Message from '../components/layout/message';
 
 function Projects() {
   return (
     <div>
       <h1>Meus Projetos</h1>
-      <Message  msg="Alguma mensagem" type={""}/>
+      <Message msg="Alguma mensagem" type="success"/>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
