ec5017793d598247bba67d9b91ee06394f2109d4
Vanessa <vs6615302@gmail.com>
2025-02-11 16:55:56 -0300

    Aplicação de estilização na page e reparando bugs no projects

--- Projetinho/src/pages/projects.tsx
+++ Projetinho/src/pages/projects.tsx
@@ -1,17 +1,21 @@
 import { useLocation } from 'react-router-dom';
 import Message from '../components/layout/message';
-import styles from '../pages/projects.module.css'; 
+import styles from '../pages/projects.module.css';
 import Container from '../components/layout/container';
 import LinkButton from '../components/layout/linkButton';
 
 function Projects() {
   const location = useLocation();
-  const message = location.state?.message || ''; 
+  const message = location.state?.message || '';
 
   return (
-    <div className={styles.teste}>
-      <h1 className={styles.tittle}>Meus Projetos</h1>
-      <LinkButton text="Novo projeto" to="/novo-projeto"/>
+    <div className={styles.projectContainer}>
+      <div className={styles.titleContainer}>
+        <h1>Meus Projetos</h1>
+
+        <LinkButton text="Criar Projeto" to="/criar-projeto" />
+      </div>
+      
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
         <p>Projetos..</p>
