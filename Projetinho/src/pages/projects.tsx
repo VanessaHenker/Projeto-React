d37cb3eed895a49be29653a58dfba86ffd91a2d1
Vanessa <vs6615302@gmail.com>
2025-02-11 16:30:51 -0300

    Alteração no erros de components

--- Projetinho/src/pages/projects.tsx
+++ Projetinho/src/pages/projects.tsx
@@ -1,25 +1,20 @@
 import { useLocation } from 'react-router-dom';
 import Message from '../components/layout/message';
-import '../pages/projects.module.css'
+import styles from '../pages/projects.module.css'; 
 import Container from '../components/layout/container';
 import LinkButton from '../components/layout/linkButton';
 
 function Projects() {
   const location = useLocation();
-  let message = '';
-
-  if (location.state) {
-    message = location.state.message;
-  }
+  const message = location.state?.message || ''; 
 
   return (
-    <div className='teste'>
-      <h1 className='tittle'>Meus Projetos</h1>
-      <LinkButton text='Novo projeto'/>
+    <div className={styles.teste}>
+      <h1 className={styles.tittle}>Meus Projetos</h1>
+      <LinkButton text="Novo projeto" to="/novo-projeto"/>
       {message && <Message type="success" msg={message} />}
-      <Container customClass='start'>
+      <Container customClass="start">
         <p>Projetos..</p>
-
       </Container>
     </div>
   );
