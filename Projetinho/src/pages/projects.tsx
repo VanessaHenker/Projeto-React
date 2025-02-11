b121912a431fec20e78475ab40d2804eac5ce301
Vanessa <vs6615302@gmail.com>
2025-02-11 16:09:21 -0300

    Alterações no project para inicialização da page

--- Projetinho/src/pages/projects.tsx
+++ Projetinho/src/pages/projects.tsx
@@ -1,6 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import Message from '../components/layout/message';
-
+import '../pages/projects.module.css'
+import Container from '../components/layout/container';
+import LinkButton from '../components/layout/linkButton';
 
 function Projects() {
   const location = useLocation();
@@ -13,7 +15,12 @@ function Projects() {
   return (
     <div className='teste'>
       <h1 className='tittle'>Meus Projetos</h1>
+      <LinkButton text='Novo projeto'/>
       {message && <Message type="success" msg={message} />}
+      <Container customClass='start'>
+        <p>Projetos..</p>
+
+      </Container>
     </div>
   );
 }
