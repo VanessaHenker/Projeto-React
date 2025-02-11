de34994b1a0d2f068d0d3f384064305a522c2e97
Vanessa <vs6615302@gmail.com>
2025-02-11 19:03:00 -0300

    Adicionando componentes para inicialização do projeto no projectCard

---
+++ Projetinho/src/components/projects/projectCard.tsx
@@ -0,0 +1,13 @@
+import styles from './projectCard.module.css'
+
+import {BsPencil, BSFillTranshfill} from 'react-icons/bs'
+
+function ProjectCard(id, name, budget, category, handleRemove) {
+  return (
+    <div>
+      <p>Projeto</p>
+    </div>
+  )
+}
+
+export default ProjectCard
