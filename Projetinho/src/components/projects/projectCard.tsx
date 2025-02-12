0d4ea065171309923d5266b4b180e2fcf5f4e411
Vanessa <vs6615302@gmail.com>
2025-02-12 19:00:56 -0300

    Aplicando estilização no projectCard

--- Projetinho/src/components/projects/projectCard.tsx
+++ Projetinho/src/components/projects/projectCard.tsx
@@ -1,13 +1,26 @@
-import styles from './projectCard.module.css'
+// src/components/projects/projectCard.jsx
+import styles from './projectCard.module.css';
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
 
-import {BsPencil, BSFillTranshfill} from 'react-icons/bs'
-
-function ProjectCard(id, name, budget, category, handleRemove) {
+function ProjectCard({ id, name, budget, category, handleRemove }) {
   return (
-    <div>
-      <p>Projeto</p>
+    <div className={styles.projectCard}>
+      <h4>{name}</h4>
+      <p>
+        <span>Orçamento:</span> R$ {budget}
+      </p>
+      <p className={styles.categoryText}>
+        <span className={`${styles[category?.toLowerCase()]}`}></span> {category}
+      </p>
+      <div className={styles.actions}>
+        <BsPencil className={styles.icon} />
+        <BsFillTrashFill
+          className={styles.icon}
+          onClick={() => handleRemove(id)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
