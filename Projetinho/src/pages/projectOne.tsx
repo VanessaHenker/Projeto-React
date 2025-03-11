8277b3114f1d25b0137dbe0bfd15ecddfef0e854
Vanessa <vs6615302@gmail.com>
2025-03-11 20:29:26 -0300

    Corrigindo erros de sintaxe e lógica no componente ProjectOne

--- Projetinho/src/pages/projectOne.tsx
+++ Projetinho/src/pages/projectOne.tsx
@@ -1,11 +1,32 @@
-/* import styles from './projectOne.module.css' */
-
-
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function ProjectOne() {
+  const { id } = useParams();
+  const [project, setProject] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setProject(data))
+      .catch((err) => console.error('Erro ao buscar o projeto:', err));
+  }, [id]);
+
+  if (!project) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <p>Projeto</p>
-  )
+    <div>
+      <h1>{project.name}</h1>
+      <p>{project.description}</p>
+    </div>
+  );
 }
 
-export default ProjectOne
+export default ProjectOne;
