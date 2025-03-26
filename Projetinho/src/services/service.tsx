3709139eb9378d571f7bb62a20d26f2790ff689f
Vanessa <vs6615302@gmail.com>
2025-03-26 20:36:50 -0300

    Adicionando componente para service

---
+++ Projetinho/src/services/service.tsx
@@ -0,0 +1,11 @@
+import styles from '../components/projects/projectForm.module.css'
+
+function Service() {
+  return (
+   <form action="">
+    <input type="text" />
+   </form>
+  )
+}
+
+export default Service
