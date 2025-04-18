ccc1142f7c9b06503fc66e20becdf184035d6bd5
Vanessa <vs6615302@gmail.com>
2025-04-18 12:26:48 -0300

    Alteração no projectCar para button edit

--- Projetinho/src/components/projects/projectCard.tsx
+++ Projetinho/src/components/projects/projectCard.tsx
@@ -112,7 +112,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           type="edit"
           label="Editar"
           iconClass={styles.icon}
-          to={`/projectOne/${id}`}
+          to={`/projetos/${id}`}
         />
         <ActionButton
           type="delete"
