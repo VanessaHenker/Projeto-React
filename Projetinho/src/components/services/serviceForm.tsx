ca88650b53a7a16f21a704fd73680cf531503ac8
Vanessa <vs6615302@gmail.com>
2025-04-23 21:49:25 -0300

    Alteração no serviceForm

--- Projetinho/src/components/services/serviceForm.tsx
+++ Projetinho/src/components/services/serviceForm.tsx
@@ -1,8 +1,43 @@
+import Input from "../form/input"
+import SubmitButton from "../form/submitButton"
 
-function ServiceForm() {
+function ServiceForm({handleSubmit, btnText, projectData}) {
+
+  function submit() {
+
+  }
+
+
+  function hendleChange(e) {
+
+  }
+
   return (
-    <form action="">
-      <input type="text" />
+    <form anSubmit {submit} className={styles.from}>
+      <Input
+        type="text"
+        text="Nome do serviço"
+        name="name"
+        placeholder="Insira o nome do serviço"
+        handleOnChange={handleChange}
+      />
+
+      <Input
+        type="number"
+        text="Custo de serviço"
+        name="cost"
+        placeholder="Insira o valor total"
+        handleOnChange={handleChange}
+      />
+
+      <Input
+        type="number"
+        text="Descrição do serviço"
+        name= "name"
+        placeholder="Descreva o serviço"
+        handleOnChange={handleChange}
+      />
+      <SubmitButton text = {texBton}/>
     </form>
   )
 }
