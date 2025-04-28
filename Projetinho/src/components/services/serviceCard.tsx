8c4c12f161300767caf32a3a3791de6c60df8b90
Vanessa <vs6615302@gmail.com>
2025-04-28 18:26:00 -0300

    Alteração no serviceCard

--- Projetinho/src/components/services/serviceCard.tsx
+++ Projetinho/src/components/services/serviceCard.tsx
@@ -1,18 +1,28 @@
 import styles from '../services/serviceCard.module.css';
 
 interface ServiceCardProps {
-  id: string;
+  id: string; 
   name: string;
   cost: string;
   description: string;
+  handleRemove: (id: string) => void;
+  handleEdit: (id: string) => void;  
 }
 
-function ServiceCard({ id, name, cost, description }: ServiceCardProps) {
+function ServiceCard({ id, name, cost, description, handleRemove, handleEdit }: ServiceCardProps) {
   return (
     <div className={styles.projectCard}>
       <h4>{name}</h4>
       <p><span>Custo total:</span> R$ {cost}</p>
       <p>{description}</p>
+
+      <button onClick={() => handleEdit(id)} className={styles.editButton}>
+        Editar Serviço
+      </button>
+
+      <button onClick={() => handleRemove(id)} className={styles.removeButton}>
+        Remover Serviço
+      </button>
     </div>
   );
 }
